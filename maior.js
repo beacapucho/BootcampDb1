49d8791f5507bdec725f2418ad2986ad22c41853
beacapucho <bsabrinaq@gmail.com>
2024-03-12 20:48:58 -0300

    feat:exibir numeros menores que 20

--- maior.js
+++ maior.js
@@ -1,6 +1,9 @@
-const arr = [9,4,5,10,2,90,8,6,33];
+const arr = [9,4,5,10,2,1,7,90,8,6,33];
 
 const maior = arr.filter((number) => number > 20);
+const menor = arr.filter((number) => number < 20);
+
 
 
 console.log(maior)
+console.log(menor)
