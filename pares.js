efcd83271dbb77b6be118de7dcb6f4c599a4d484
beacapucho <bsabrinaq@gmail.com>
2024-03-12 19:45:32 -0300

    refatorando o arquivo "pares.js"

--- pares.js
+++ pares.js
@@ -1,5 +1,5 @@
 const array = [2,4,3,1,6,7,9,10]
 
-const pares = array.filter(items => items % 2 === 0)
+const Numpares = array.filter(items => items % 2 === 0);
 
-console.log("Os números são: " pares);
+console.log("Os números pares são: ", Numpares);
